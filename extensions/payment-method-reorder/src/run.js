e06fd90ca48d887c56f4b77544dc70ad26c782c2
MehediHasan06 <mehedihasanmasud06@gmail.com>
2024-04-25 11:56:51 +0600

    add payment method reorder shopify function extension

---
+++ extensions/payment-method-reorder/src/run.js
@@ -0,0 +1,53 @@
+// @ts-check
+
+
+// Use JSDoc annotations for type safety
+/**
+* @typedef {import("../generated/api").RunInput} RunInput
+* @typedef {import("../generated/api").FunctionRunResult} FunctionRunResult
+*/
+
+/**
+* @type {FunctionRunResult}
+*/
+const NO_CHANGES = {
+  operations: [],
+};
+
+// The configured entrypoint for the 'purchase.payment-customization.run' extension target
+/**
+* @param {RunInput} input
+* @returns {FunctionRunResult}
+*/
+export function run(input) {
+  // Get the cart total from the function input, and return early if it's below 100
+  // const cartTotal = parseFloat(input.cart.cost.totalAmount.amount ?? "0.0");
+  // if (cartTotal < 100) {
+  //   // You can use STDERR for debug logs in your function
+  //   console.error("Cart total is not high enough, no need to hide the payment method.");
+  //   return NO_CHANGES;
+  // }
+
+  // Find the payment method to hide
+  const targetPaymentMethod = input.paymentMethods
+    .find(method => method.name.includes("Bank Deposit"));
+
+  if (!targetPaymentMethod) {
+    console.error('No method found as the targetPaymentMethod', targetPaymentMethod)
+    return NO_CHANGES;
+  }
+
+  // The @shopify/shopify_function package applies JSON.stringify() to your function result
+  // and writes it to STDOUT
+  return {
+    operations: [{
+      // hide: {
+      //   paymentMethodId: hidePaymentMethod.id
+      // }
+      move: {
+        index: 0,
+        paymentMethodId: targetPaymentMethod.id
+      }
+    }]
+  };
+};
